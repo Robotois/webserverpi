13b31a2a8374b5de95f660679f1e5ecc5d1de2dd
Armando Gonzalez <iam@armando.mx>
2016-06-14 16:20:47 -0500

    Initial commit for rest api

---
+++ controllers/service.js
@@ -0,0 +1,6 @@
+module.exports.home = function(request, response) {
+  response.send('Import Service API');
+};
+module.exports.postData = function(request, response) {
+  response.send('Import Service API');
+};
---
+++ index.js
@@ -0,0 +1,26 @@
+//var service = require('./controllers/service.js');
+var logger = require('koa-logger');
+var route = require('koa-route');
+var parse = require('co-body');
+var koa = require('koa');
+var app = koa();
+// middleware
+
+app.use(logger());
+
+app.use(route.get('/', home));
+app.use(route.post('/post', show));
+
+function *home() {
+  this.body = 'Hello World';
+}
+
+function *show() {
+   var data = yield parse(this);
+   this.body = data;
+}
+
+// listen
+
+app.listen(8082);
+console.log('listening on port 8082');
