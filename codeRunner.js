2ffc20ec871e5275a63f855b6f7c54e45d33f1d4
Armando Gonzalez <iam@armando.mx>
2017-02-24 16:04:34 -0600

    refactor exit process code

--- codeRunner.js
+++ codeRunner.js
@@ -48,20 +48,47 @@ if (modules.ledRGB && modules.ledRGB.port) {
 
 eval(data.code);
 
-/*setInterval(()=>{ // Proceso en estado ocioso
+setInterval(()=>{ // Proceso en estado ocioso
   true;
-}, 10000);*/
+}, 10000);
 
-process.on('SIGTERM', function () {
-  led.release();
+function exitHandler(options, err) {
+  // light sensor
+  if (modules.light && modules.light.port) {
+    light.release();
+  }
+  // led
+  if (modules.led && modules.led.port) {
+    led.release();
+  }
+  // temperature
+  if (modules.temperature && modules.temperature.port) {
+    temperature.release();
+  }
+  // lcd
+  if (modules.lcd && modules.lcd.port) {
+    lcd.release();
+  }
+  // rotatory
+  if (modules.rotatory && modules.rotatory.port) {
+    rotatory.release();
+  }
+  // distance
+  if (modules.distance && modules.distance.port) {
+    distance.release();
+  }
+  // button
+  if (modules.button && modules.button.port) {
+    button.release();
+  }
+  // ledRGB
+  if (modules.ledRGB && modules.ledRGB.port) {
+    ledRGB.release();
+  }
   process.exit();
-});
+}
 
-process.on('SIGINT', function () {
-  led.release();
-  process.exit();
-});
-process.on('exit', function () {
-  led.release();
-  console.log('exit');
-});
+
+process.on('SIGTERM', exitHandler);
+process.on('SIGINT', exitHandler);
+process.on('exit', exitHandler);
