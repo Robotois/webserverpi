ff50b0bd92a81f2f71ec72b2813c0c41c7693366
Armando Gonzalez <iam@armando.mx>
2017-03-23 22:29:29 -0600

    fixed typo issues

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ module.exports = function App(wifiManager) {
   app.use(route.post('/post', post));
   app.use(route.post('/reset', reset));
   app.use(route.post('/enable-wifi', enableWifi));
-  app.user(route.post('/rescan-wifi', rescanWifi));
+  app.use(route.post('/rescan-wifi', rescanWifi));
 
   // listen
   app.listen(8082);
