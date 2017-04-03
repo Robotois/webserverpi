935f81b605d8a8bf7527391f93cd8738f15850cd
Armando Gonzalez <iam@armando.mx>
2017-04-03 18:36:48 -0500

    Added enable wifi method

--- accessPoint.js
+++ accessPoint.js
@@ -1,10 +1,11 @@
 const logger = require('koa-logger');
 const route = require('koa-route');
+const parse = require('co-body');
 const koa = require('koa');
 const config = require('./config.json');
 const iwList = require('./lib/iwList');
 
-module.exports = function App() {
+module.exports = function App(wifiManager) {
   const app = koa();
 
   app.use(logger());
@@ -22,6 +23,37 @@ module.exports = function App() {
     });
   }
   app.use(route.post('/list-wifis', listWifis));
+
+  function* enableWifi() {
+    const data = yield parse(this);
+    const connInfo = {
+      wifi_ssid: data.ssid,
+      wifi_passcode: data.passcode,
+    };
+    console.log(connInfo);
+    // TODO: If wifi did not come up correctly, it should fail
+    // currently we ignore ifup failures.
+    this.body = yield new Promise((resolve, reject) => {
+      wifiManager.enable_wifi_mode(connInfo, (error) => {
+        if (error) {
+          console.log(`Enable Wifi ERROR: ${error}`);
+          console.log('Attempt to re-enable AP mode');
+          wifiManager.enable_ap_mode(config.access_point.ssid, (error2) => {
+            console.log(error2);
+            console.log('... AP mode reset');
+          });
+          reject(error);
+        } else {
+          console.log('Wifi Enabled! - Exiting');
+          resolve({
+            success: true,
+            message: 'Wifi Enabled! - Exiting',
+          });
+        }
+      });
+    });
+  }
+  app.use(route.post('/enable-wifi', enableWifi));
 
   // listen
   app.listen(config.server.port);
