5cc7e0b8142af0f6773cdbf3e8dd734124b7b6cb
Armando Gonzalez <iam@armando.mx>
2017-04-27 19:19:42 -0500

    to test wifi con error

--- wifi.js
+++ wifi.js
@@ -1,12 +1,14 @@
-const wpaSupplicant = require('wireless-tools/wpa_supplicant');
+const WiFiControl = require('wifi-control');
 
-const options = {
-  interface: 'wlan0',
-  ssid: 'ARRIS-3882',
-  passphrase: '12344566456',
-  driver: 'nl80211'
-};
-wpaSupplicant.enable(options, (err) => {
-  console.log(arguments);
-  console.log(err);
-});
+WiFiControl.connectToAP(
+  {
+    ssid: 'ARRIS-3882',
+    passphrase: '12344566456'
+  },
+  (err, response) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(response);
+  }
+);
