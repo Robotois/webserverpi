b9d0224170d830f38942879c6ec62dcda15602e7
Armando Gonzalez <iam@armando.mx>
2017-04-30 17:02:34 -0500

    tests with node wifi

--- wifi.js
+++ wifi.js
@@ -1,13 +1,14 @@
-const WiFiControl = require('wifi-control');
+// const WiFiControl = require('wifi-control');
+const WiFiControl = require('node-wifi');
 
-const settings = {
+/* const settings = {
   debug: true,
   iface: 'wlan0',
   connectionTimeout: 10000 // in ms
 };
 
-WiFiControl.configure(settings);
-WiFiControl.init({ debug: true });
+WiFiControl.configure(settings);*/
+WiFiControl.init({ iface: 'wlan0' });
 WiFiControl.connectToAP(
   {
     ssid: 'ARRIS-3882',
