c2f576791f7f7d365db45023bfe1a679292a15e2
Armando Gonzalez <iam@armando.mx>
2017-05-02 18:52:05 -0500

    another way to do it

--- wifi.js
+++ wifi.js
@@ -1,23 +1,50 @@
-// const WiFiControl = require('wifi-control');
-const WiFiControl = require('node-wifi');
+const wpaSupplicant = require('wireless-tools/wpa_supplicant');
+const exec = require('child_process').exec;
 
-/* const settings = {
-  debug: true,
-  iface: 'wlan0',
-  connectionTimeout: 10000 // in ms
+const MAX_WAIT_TIME = 20000;
+const exitIfReady = function exitIfReady() {
+  exec('hostname -I', (err2, output) => {
+    if (output.trimRight()) {
+      // Exit as soon as this succeeds
+      process.exit(0);
+    }
+  });
 };
 
-WiFiControl.configure(settings);*/
-WiFiControl.init({ iface: 'wlan0' });
-WiFiControl.connect(
-  {
-    ssid: 'ARRIS-3882',
-    passphrase: '11A10DA43FCEBC11'
-  },
-  (err, response) => {
-    if (err) {
-      console.log(err);
-    }
-    console.log(response);
+const options = {
+  interface: 'wlan0',
+  ssid: 'ARRIS-3882',
+  passphrase: '11A10DA43FCEBC11',
+  driver: 'nl80211'
+};
+
+wpaSupplicant.enable(options, (error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    exec('ifdown wlan0 && sleep 1 && ifup wlan0', (err, response) => {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      } else {
+        // Every 2 seconds, check to see if it succeeded
+        for (let k = 2000; k < MAX_WAIT_TIME; k += 2000) {
+          setTimeout(
+            () => {
+              exitIfReady();
+            },
+            k
+          );
+        }
+
+        setTimeout(
+          () => {
+            console.error('Unable to get an IP address');
+            process.exit(1);
+          },
+          MAX_WAIT_TIME
+        );
+      }
+    });
   }
-);
+});
