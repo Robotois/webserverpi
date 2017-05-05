caba18025a0981bf59ab9258f006b526005d18c5
Armando Gonzalez <iam@armando.mx>
2017-05-04 23:01:44 -0500

    Updated wifi pass implementation

--- accessPoint.js
+++ accessPoint.js
@@ -4,21 +4,27 @@ const parse = require('co-body');
 const koa = require('koa');
 const config = require('./config.json');
 const iwlist = require('wireless-tools/iwlist');
-const wpaSupplicant = require('wireless-tools/wpa_supplicant');
 const setHostName = require('./lib/hostname');
 const exec = require('child_process').exec;
+const fs = require('fs');
+const path = require('path');
 
 const MAX_WAIT_TIME = 20000;
+const SUPPLICANT_PATH = path.join('/etc', 'wpa_supplicant', 'wpa_supplicant.conf');
 
 const exitIfReady = function exitIfReady(hostname) {
   let newIP;
   exec('hostname -I', (err2, output) => {
     newIP = output.trimRight();
     newIP = newIP.split(' ');
-    if (newIP.length > 1) {
+    if (newIP.length) {
       setHostName(hostname || 'robotois01');
     }
   });
+};
+
+const getHeadOfConf = function getHeadOfConf(filePath) {
+  return fs.readFileSync(filePath).toString().replace(/network(.|\n)*$/, '');
 };
 
 
@@ -49,41 +55,39 @@ module.exports = function App() {
    */
   function* enableWifi() {
     const data = yield parse(this);
-    const options = {
-      interface: 'wlan0',
-      ssid: data.ssid,
-      passphrase: data.passcode,
-      driver: 'nl80211'
-    };
 
     this.body = yield new Promise((resolve, reject) => {
-      wpaSupplicant.enable(options, (error) => {
-        if (error) {
-          reject(error);
-        } else {
-          exec('ifdown wlan0 && sleep 1 && ifup wlan0', (err2) => {
-            if (err2) {
-              reject(err2);
-            } else {
-              // Every 2 seconds, check to see if it succeeded
-              for (let k = 2000; k < MAX_WAIT_TIME; k += 2000) {
-                setTimeout(
-                  () => {
-                    exitIfReady(data.hostname);
-                  },
-                  k
-                );
-              }
+      const templateConf = getHeadOfConf(SUPPLICANT_PATH);
+      const networkString = `network={\n
+          \tssid="${data.ssid}"\n
+          \tpsk="${data.passcode}"\n
+        }\n`;
 
-              setTimeout(
-                () => {
-                  console.error('Unable to get an IP address');
-                  reject('Unable to get an IP address');
-                },
-                MAX_WAIT_TIME
-              );
-            }
-          });
+      fs.writeFileSync(SUPPLICANT_PATH, templateConf + networkString);
+
+      console.log('Restarting network connection');
+      exec('ifdown wlan0 && sleep 1 && ifup wlan0', (err) => {
+        if (err) {
+          console.error(err);
+          reject(err);
+        } else {
+          // Every 2 seconds, check to see if it succeeded
+          for (let k = 2000; k < MAX_WAIT_TIME; k += 2000) {
+            setTimeout(
+              () => {
+                exitIfReady(data.hostname);
+              },
+              k
+            );
+          }
+
+          setTimeout(
+            () => {
+              console.error('Unable to get an IP address');
+              reject('Unable to get an IP address');
+            },
+            MAX_WAIT_TIME
+          );
         }
       });
     });
